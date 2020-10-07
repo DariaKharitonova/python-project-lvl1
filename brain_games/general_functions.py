36b4b457fb7510abb3b44b0e84b9fb05b12bdef2
Daria <kharitonovadari@gmail.com>
2020-10-07 11:52:54 +0300

    fix linter errors

--- brain_games/general_functions.py
+++ brain_games/general_functions.py
@@ -41,7 +41,8 @@ def play_game(user_name, play):
         user_answer = get_user_answer()
         is_correct = is_answer_correct(user_answer, correct_answer)
         if not is_correct:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print([
+                f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'."])
             print(f"Let's try again, {user_name}!")
             return
         print('Correct!')
