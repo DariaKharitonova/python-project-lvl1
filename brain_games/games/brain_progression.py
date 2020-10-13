bb49423ceb6d963051c353fd065c2339dd9c8cb8
Daria <kharitonovadari@gmail.com>
2020-10-13 23:38:11 +0300

    fix 4 review errors

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -8,19 +8,15 @@ MIN_PROGRESSION_STEP = 1
 MAX_PROGRESSION_STEP = 10
 
 
-def get_elements_progression():
-    first_number = random.randint(MIN_NUMBER, MIN_NUMBER)
+def create_progression():
+    first_number = random.randint(MIN_NUMBER, MAX_NUMBER)
     step = random.randint(MIN_PROGRESSION_STEP, MAX_PROGRESSION_STEP)
     max_number = first_number + (PROGRESSION_LENGHT * step)
-    return first_number, step, max_number
-
-
-def create_progression(first_number, max_number, step):
     return range(first_number, max_number, step)
 
 
 def get_question_and_answer():
-    progression = create_progression(first_number, max_number, step)
+    progression = create_progression()
     hidden_element_index = random.randint(0, len(progression) - 1)
     hidden_element = progression[hidden_element_index]
     result_progression = ' '.join([
