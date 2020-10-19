da16584f4c1c935660b108d48ae58eceab04f5f6
Daria <kharitonovadari@gmail.com>
2020-10-19 19:35:40 +0300

    final refactoring

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -13,12 +13,8 @@ def get_question_and_answer():
     second_number = generate_number()
     symbol, operation = generate_operation()
     task = f'{first_number} {symbol} {second_number}'
-    answer = get_correct_answer(first_number, operation, second_number)
+    answer = str(operation(first_number, second_number))
     return task, answer
-
-
-def get_correct_answer(first_number, operation_symbol, second_number):
-    return str(operation_symbol(first_number, second_number))
 
 
 def generate_operation():
